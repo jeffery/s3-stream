ab928897c2652b39bd249e8b8fd28a6488888695
Jesse Schalken <me@jesseschalken.com>
2014-12-02 00:43:42 +1100

    Finished

---
+++ s3-stream.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace S3Stream;
+
+use Aws\Common\Enum\ClientOptions;
+use Aws\S3\S3Client;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+try {
+    date_default_timezone_set('Australia/Melbourne');
+
+    $args = \Docopt::handle(<<<'s'
+Usage:
+  s3-stream.php [options] bucket list
+  s3-stream.php [options] bucket create <bucket> [--location=<location>]
+  s3-stream.php [options] bucket delete <bucket>
+  s3-stream.php [options] object read <bucket> <key>
+  s3-stream.php [options] object write <bucket> <key>
+  s3-stream.php [options] object delete <bucket> <keys>...
+  s3-stream.php [options] object list <bucket> [--prefix=<prefix>]
+
+Options:
+  --key=<key>             AWS access key ID
+  --secret=<secret>       AWS access key secret
+  --region=<region>       AWS region
+  --base-url=<base-url>   Override "https://s3.amazonaws.com" base URL
+  --verbose               Enable verbose logging
+  --prefix=<prefix>       To only list the objects with the specified prefix
+  --location=<location>   Create the bucket with this location. Usually this needs to be set the same as --region.
+s
+    );
+
+    $params = array();
+
+    if (isset($args['--key']))
+        $params[ClientOptions::KEY] = $args['--key'];
+    if (isset($args['--secret']))
+        $params[ClientOptions::SECRET] = $args['--secret'];
+    if (isset($args['--region']))
+        $params[ClientOptions::REGION] = $args['--region'];
+    if (isset($args['--base-url']))
+        $params[ClientOptions::BASE_URL] = $args['--base-url'];
+
+    $s3 = new S3(S3Client::factory($params));
+
+    if ($args['--verbose'])
+        $s3->addLogger(function ($x) { fwrite(STDERR, $x); });
+
+    /**
+     * @param string[] $lines
+     * @return string
+     */
+    function joinLines(array $lines) {
+        return $lines ? join("\n", $lines) . "\n" : '';
+    }
+
+    if ($args['bucket']) {
+        if ($args['list'])
+            print joinLines($s3->listBuckets());
+        if ($args['create'])
+            $s3->createBucket($args['<bucket>'], $args['--location']);
+        if ($args['delete'])
+            $s3->deleteBucket($args['<bucket>']);
+    }
+
+    if ($args['object']) {
+        $bucket = $args['<bucket>'];
+
+        if ($args['read'])
+            $s3->readObject($bucket, $args['<key>'], STDOUT);
+        if ($args['write'])
+            $s3->writeObject($bucket, $args['<key>'], STDIN);
+        if ($args['list'])
+            print joinLines($s3->listKeys($bucket, $args['--prefix']));
+        if ($args['delete'])
+            $s3->deleteKeys($bucket, $args['<keys>']);
+    }
+} catch (\Exception $e) {
+    fwrite(STDERR, $e->getMessage() . "\n");
+    exit(1);
+}
---
+++ s3.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace S3Stream;
+
+use Aws\S3\S3Client;
+use Guzzle\Http\EntityBody;
+use Guzzle\Log\ClosureLogAdapter;
+use Guzzle\Log\MessageFormatter;
+use Guzzle\Plugin\Log\LogPlugin;
+
+class S3 {
+    private $s3;
+
+    function __construct(S3Client $s3) {
+        $this->s3 = $s3;
+    }
+
+    function addLogger(\Closure $closure) {
+        $this->s3->addSubscriber(new LogPlugin(new ClosureLogAdapter($closure), MessageFormatter::SHORT_FORMAT));
+    }
+
+    /**
+     * @return \string[]
+     */
+    function listBuckets() {
+        $results = array();
+        $buckets = $this->s3->listBuckets();
+        foreach ($buckets['Buckets'] as $bucket)
+            $results[] = $bucket['Name'];
+        return $results;
+    }
+
+    /**
+     * @param string $bucket
+     * @param string|null $location
+     */
+    function createBucket($bucket, $location = null) {
+        $params = array('Bucket' => $bucket);
+        if ($location !== null)
+            $params['LocationConstraint'] = $location;
+        $this->s3->createBucket($params);
+    }
+
+    /**
+     * @param string $bucket
+     */
+    function deleteBucket($bucket) {
+        $this->s3->deleteBucket(array('Bucket' => $bucket));
+    }
+
+    /**
+     * @param string $bucket
+     * @param string $prefix
+     * @return \string[]
+     */
+    function listKeys($bucket, $prefix = '') {
+        $keys = array();
+        $params = array('Bucket' => $bucket, 'Prefix' => $prefix);
+        while (true) {
+            $response = $this->s3->listObjects($params);
+
+            if (isset($response['Contents']))
+                foreach ($response['Contents'] as $object)
+                    $keys[] = $object['Key'];
+
+            if ($response['IsTruncated'])
+                $params['Marker'] = $response['NextMarker'];
+            else
+                break;
+        }
+        return $keys;
+    }
+
+    /**
+     * @param string $bucket
+     * @param string[] $keys
+     */
+    function deleteKeys($bucket, $keys) {
+        $objects = array();
+        foreach ($keys as $key)
+            $objects[] = array('Key' => $key);
+        $this->s3->deleteObjects(array(
+            'Bucket' => $bucket,
+            'Objects' => $objects,
+        ));
+    }
+
+    /**
+     * @param string $bucket
+     * @param string $key
+     * @param resource $resource A stream to read the object into
+     */
+    function readObject($bucket, $key, $resource) {
+        $request = $this->s3->getCommand('GetObject', array(
+            'Bucket' => $bucket,
+            'Key' => $key,
+        ))->prepare();
+        $request->setResponseBody(new EntityBody($resource));
+        $request->send();
+    }
+
+    /**
+     * @param string $bucket
+     * @param string $key
+     * @return S3Upload
+     */
+    function newUpload($bucket, $key) {
+        return new S3Upload($this->s3, $bucket, $key);
+    }
+
+    /**
+     * @param string $bucket
+     * @param string $key
+     * @param resource $resource A stream to read the object from
+     */
+    function writeObject($bucket, $key, $resource) {
+        $upload = $this->newUpload($bucket, $key);
+        while (!feof($resource))
+            $upload->add(fread($resource, 5 * 1024 * 1024));
+        $upload->finish();
+    }
+}
+
+class S3Upload {
+    private $s3;
+    private $uploadId, $parts = array(), $partNumber = 1, $buffer = '';
+    private $bucket, $key;
+    private $done = false;
+
+    /**
+     * @param S3Client $s3
+     * @param string $bucket
+     * @param string $key
+     */
+    function __construct(S3Client $s3, $bucket, $key) {
+        $response = $s3->createMultipartUpload(array(
+            'Bucket' => $bucket,
+            'Key' => $key,
+        ));
+
+        $this->s3 = $s3;
+        $this->bucket = $bucket;
+        $this->key = $key;
+        $this->uploadId = $response['UploadId'];
+    }
+
+    function __destruct() {
+        if (!$this->done) {
+            $this->s3->abortMultipartUpload(array(
+                'Bucket' => $this->bucket,
+                'Key' => $this->key,
+                'UploadId' => $this->uploadId,
+            ));
+        }
+    }
+
+    /**
+     * @param string $data
+     */
+    function add($data) {
+        $this->buffer .= $data;
+
+        if (strlen($this->buffer) >= 5 * 1024 * 1024)
+            $this->sendBuffer();
+    }
+
+    function finish() {
+        if ($this->done)
+            throw new \Exception("this upload has already finished");
+
+        if (strlen($this->buffer) > 0)
+            $this->sendBuffer();
+
+        $this->s3->completeMultipartUpload(array(
+            'Bucket' => $this->bucket,
+            'Key' => $this->key,
+            'Parts' => $this->parts,
+            'UploadId' => $this->uploadId,
+        ));
+
+        $this->done = true;
+    }
+
+    private function sendBuffer() {
+        $partNumber = $this->partNumber++;
+        $response = $this->s3->uploadPart(array(
+            'Bucket' => $this->bucket,
+            'Key' => $this->key,
+            'PartNumber' => $partNumber,
+            'UploadId' => $this->uploadId,
+            'Body' => $this->buffer,
+        ));
+        $this->buffer = '';
+        $this->parts[] = array(
+            'ETag' => $response['ETag'],
+            'PartNumber' => $partNumber,
+        );
+    }
+}
